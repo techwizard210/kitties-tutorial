142332c222e599a31e96e9c7327c9644397c7056
Worlddev <worlddev5007@gmail.com>
2022-06-08 00:42:46 +0900

    Changed service.rs 2022.6.8

--- node/src/service.rs
+++ node/src/service.rs
@@ -15,22 +15,22 @@ use std::{sync::Arc, time::Duration};
 // Our native executor instance.
 pub struct ExecutorDispatch;
 
-// impl sc_executor::NativeExecutionDispatch for ExecutorDispatch {
-// 	/// Only enable the benchmarking host functions when we actually want to benchmark.
-// 	#[cfg(feature = "runtime-benchmarks")]
-// 	type ExtendHostFunctions = frame_benchmarking::benchmarking::HostFunctions;
-// 	/// Otherwise we only use the default Substrate host functions.
-// 	#[cfg(not(feature = "runtime-benchmarks"))]
-// 	type ExtendHostFunctions = ();
+impl sc_executor::NativeExecutionDispatch for ExecutorDispatch {
+	/// Only enable the benchmarking host functions when we actually want to benchmark.
+	#[cfg(feature = "runtime-benchmarks")]
+	type ExtendHostFunctions = frame_benchmarking::benchmarking::HostFunctions;
+	/// Otherwise we only use the default Substrate host functions.
+	#[cfg(not(feature = "runtime-benchmarks"))]
+	type ExtendHostFunctions = ();
 
-// 	fn dispatch(method: &str, data: &[u8]) -> Option<Vec<u8>> {
-// 		node_kitties_runtime::api::dispatch(method, data)
-// 	}
+	fn dispatch(method: &str, data: &[u8]) -> Option<Vec<u8>> {
+		node_kitties_runtime::api::dispatch(method, data)
+	}
 
-// 	fn native_version() -> sc_executor::NativeVersion {
-// 		node_kitties_runtime::native_version()
-// 	}
-// }
+	fn native_version() -> sc_executor::NativeVersion {
+		node_kitties_runtime::native_version()
+	}
+}
 
 type FullClient =
 	sc_service::TFullClient<Block, RuntimeApi, NativeElseWasmExecutor<ExecutorDispatch>>;
